58a0eb8edfec51e0da3d51faceb758cefd3c3885
KiwiTech <kiwitech@KT-MAC-039.local>
2019-08-26 19:01:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-function App() {
+import Blogs from './components/blogs';
+import New from './components/blogs';
+import Side from './components/navbar'
+
+const App = () => {
+
+
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Side />
+          <Switch>
+            <Route exact path="/" component={Blogs} />
+            <Route path="/new" component={New} />
+          </Switch>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
